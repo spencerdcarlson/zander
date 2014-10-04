e2c14ccc052a43968516a8636ef80cf08ee077e9
Spencer Carlson <spencerdcarlson@gmail.com>
2014-10-04 14:21:47 -0500

    v1

---
+++ README.md
@@ -0,0 +1,12 @@
+Script to automaticlly log in a user to http://www.lynda.com
+
+### Set Up ###
+1.) Install ruby
+    1.1) See http://bit.ly/rbenvBrew to install with rbenv via homebrew
+2.) Install bundler gem 
+    2.1) $ gem install bundler
+3.) $ cd logIn
+4.) $ bundle install
+5.) edit config.yaml with your lynda.com credentials
+6.) $ ruby log_in.rb
+7.) Set cron job to execute script
---
+++ log_in.rb
@@ -0,0 +1,52 @@
+require 'selenium-webdriver'
+require 'yaml'
+
+driver = Selenium::WebDriver.for :firefox
+wait = Selenium::WebDriver::Wait.new(:timeout => 10) 
+
+def get_fully_qualified_path(file_name)
+	return File.join File.expand_path(File.dirname(__FILE__)), file_name
+end
+
+def get_properties()
+	config = YAML::load(File.read(get_fully_qualified_path('config.yaml')))
+	if config.has_key?('LOGIN') && config.has_key?('PASSWORD')
+		@login = config['LOGIN'][0] # get the first value for LOGIN
+		@passwd = config['PASSWORD'][0] # get the frist value for PASSWORD
+	end
+	if config.has_key?('FIRST_NAME')
+		@first_name = config['FIRST_NAME'][0]
+	end
+end
+
+begin
+	get_properties()
+	driver.navigate.to "http://lynda.com"
+	login_modal = wait.until { driver.find_element(:id => "login-modal") }
+	login_modal.click
+	driver.switch_to.frame "fancybox-frame"
+	usernameInput = wait.until { driver.find_element(:id => "usernameInput") }
+	passwordInput = wait.until { driver.find_element(:id => "passwordInput") }
+	lnk_login = wait.until { driver.find_element(:id => "lnk_login") }
+	usernameInput.send_keys @login if @login
+	passwordInput.send_keys @passwd if @passwd
+	lnk_login.click
+	guided_tour_cancel = wait.until { driver.find_element(:id => "guided-tour-cancel") }
+	puts "Logged In"
+	guided_tour_cancel.click
+	loggedInUser = wait.until { driver.find_element(:partial_link_text => 'Hi,') }
+	user = loggedInUser.text; user.slice! 'Hi,'
+	if user && @first_name
+		if user.strip.upcase == @first_name.upcase
+			puts "User: #{user.capitalize}"
+		else
+			puts "Wrong user!"
+		end
+	end
+	loggedInUser.click
+	log_out = wait.until { driver.find_element(:partial_link_text => 'Log out') }
+	log_out.click
+	puts "Logged Out"
+ensure
+	driver.quit
+end
