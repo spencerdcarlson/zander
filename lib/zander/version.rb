a8f8b4e109e9192d2dd9dbf4392fafa9c0044c74
Spencer Carlson <spencerdcarlson@gmail.com>
2014-11-16 22:11:45 -0600

    version

--- lib/zander/version.rb
+++ lib/zander/version.rb
@@ -1,4 +1,5 @@
 module Zander
 	#VERSION = '0.1.0' # initial release
-	VERSION = '0.1.1' # fiexed binary and added documentation
+	#VERSION = '0.1.1' # fiexed binary and added documentation
+	VERSION = '0.1.2' # updated gemspec
 end
